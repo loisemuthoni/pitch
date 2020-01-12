7389c7b564c810ecb778bbd1130a2a4622b3c6ea
Moringa School <loisemburu01@gmail.com>
2020-01-13 00:36:59 +0300

    created home &about page

--- flaskblog.py
+++ flaskblog.py
@@ -3,5 +3,16 @@ app = Flask (__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About page</h1>"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
