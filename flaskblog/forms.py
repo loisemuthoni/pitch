3811fed56fec6d8474baa73379314cce9f896b6d
Moringa School <loisemburu01@gmail.com>
2020-01-14 18:03:17 +0300

    created a profile pic

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -12,14 +13,16 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
     
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('That username is taken. Please choose another one ')
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose another one ')
 
     def validate_username(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose another one ')
+           if email.data != current_user.email:
+                 user = User.query.filter_by(email=email.data).first()
+                 if user:
+                    raise ValidationError('That email is taken. Please choose another one ')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -27,9 +30,10 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me') 
     submit = SubmitField('Update')
 
-    class UpdateAccountForm(FlaskForm):
+class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
     submit = SubmitField('Sign Up')
     
     def validate_username(self, username):
